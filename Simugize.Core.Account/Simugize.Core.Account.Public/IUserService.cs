77ea5c0ab40028cc5746bb790dc1bb6c48ddc678
simugize <rmillsap@simugize.com>
2024-03-22 05:41:44 -0700

    Created IUserService

---
+++ Simugize.Core.Account/Simugize.Core.Account.Public/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace Simugize.Core.Account.Public
+{
+    public class IUserService
+    {
+        //IApiResponse<IUser> CreateUserAsync(CreateUserRequest req);
+        //IApiResponse<IUser> FetchUserAsync(FetchUserRequest req);
+        //IApiResponse<IResultSet<IUser>> SearchUsersAsync(SearchUsersRequest req);
+    }
+}
